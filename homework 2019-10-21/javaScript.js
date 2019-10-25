9c45a6b12ef45c0f20adea12dd0b26aeb3528fe1
alexanderLP-dev <golovninalexandr87@gmail.com>
2019-10-25 12:06:17 +0300

    HW01

---
+++ homework 2019-10-21/javaScript.js
@@ -0,0 +1,12 @@
+"use strict";
+// Задача 1
+
+// Найдите и исправьте ошибки в коде:
+
+var x = +parseInt(prompt("Введите x", "1 "));
+var y = +parseInt(prompt("Введите y", "1 "));
+
+var x = +prompt("Введите x", "1");
+var y = +prompt("Введите y", "1");
+
+alert("Сумма:" + (x + y));
