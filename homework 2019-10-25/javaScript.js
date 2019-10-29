ae8cfb96c1f67da96199285fc8a337c1292e5ac1
alexanderLP-dev <golovninalexandr87@gmail.com>
2019-10-29 21:36:31 +0200

    homework07

--- homework 2019-10-25/javaScript.js
+++ homework 2019-10-25/javaScript.js
@@ -1,7 +1,21 @@
 "use strict";
-//task 1  !!!!!!!!!!!!!!!!!!
+//task 1
+// Создайте блок - схему, описывающую алгоритм поиска минимального из трех чисел.
+// Запишите алгоритм программно:
+// создайте три переменные с любыми числовыми значениями;
+// используя только вложенные условные операторы if (НЕ логические операторы),
+// найдите минимальное число и отобразите на экране его значение.
+
+// Схема в HTML!!!
 
 //Task 2
+
+// По склону Фудзи ползет улитка.
+// Ее скорость определяется в диалоговом окне.
+// Проверить и вывести на экран, улитка двигается вперед,
+// назад или остается на месте(скорость(вектор) положительная,
+// отрицательная или ноль).
+
 // let inputSpeed = prompt("Введите скорость улитки : ");
 // let speed = "При скорости " + inputSpeed + "км\\с";
 // if (inputSpeed < 0) {
@@ -14,6 +28,14 @@
 // }
 
 //task 3
+
+// У вас и у вашего друга в кармане столько денег,
+// сколько было указано в окнах prompt.
+// В зависимости от общей суммы ваших сбережений вы можете полететь
+// на Мальту изучать английский язык или выпить пива.
+// С помощью условного оператора определите ваши возможности
+// и отобразите на экране в alert.
+
 // debugger;
 // let moneyPetr = parseFloat(+prompt("Введите кол-во денег Петра : " + "грн"));
 // let moneyVasya = +prompt("Введите кол-во денег Васи : " + "грн");
@@ -31,12 +53,22 @@
 // }
 
 // task 4
+
+// В переменную age запишите возраст человека.
+// Если значение больше или равно 20 и меньше 30,
+// в alert выводится “Выслать повестку”.
+
 // let age = 31;
 // if (age >= 20 && age < 30) {
 //   alert("Упс вы выписали себе повестку в армию и переписали  квартиру!))");
 // } else alert("С вами все в порядке");
 
 // task 5
+
+// Чтобы добраться домой,
+// вас устраивают маршрутки номер 7, 225 и 255.
+// Какая маршрутка приехала – определяется в prompt.
+// Если ваша, то вы едете домой, в противном случае – ожидаете.
 
 // var suitable = +prompt(`Подходящие маршрутки : 7, 225, 255
 // Введите какая приехала: `);
@@ -47,38 +79,53 @@
 // }
 
 // task 6
+// В переменную day записан текущий день недели.
+// Если это не суббота и не воскресенье,
+// выведите в alert сентенцию о необходимости идти на работу.
 // переменные пробовал и в if присваивать тоже получалось
+
 // let a, b;
-// a = `saturday`;
-// b = `sunday`;
+// a = "saturday";
+// b = "sunday";
 // let day = b;
 
 // if (day != a && day != b) {
-//   alert(`на работу!`);
+//   alert("на работу!");
 // } else {
-//   alert(`выходной`);
+//   alert("выходной");
 // }
 
 // task 7
-// debugger;
-// var name = prompt(`Введите ваше имя : `);
 
-// if (name !== "null" && name !== "") {
+// Получите из prompt значение для имени пользователя.
+// Выведите на экран приветствие.Учтите вариант,
+// что пользователь может не ввести ничего или нажать на Отмена.
+
+// debugger;
+// const name = prompt("Введите ваше имя : ");
+// if (name == null) {
+//   alert("Капец!");
+// } else if (name != Number(name)) {
 //   alert("Я Вас приветствую " + name + "!");
 // } else {
-//   alert(`write somthing`);
+//   alert("write something");
 // }
 
 // task 8
+
+// Напишите калькулятор обмена валюты USD - UAH.
+// Из первого prompt читается тип операции(регистр символов не имеет значения),
+// из второго - сумма, подлежащая конвертации.Используя switch,
+// рассчитайте результат конвертации.Результат выведите его в alert.
 
 // let typeOperation = prompt(`Введите тип операции :
 // 1)USD - UAH
 // 2)UAH - USD`);
 // let a, b;
-// a = `usd-uah`;
-// b = `uah-usd`;
+// a = "usd-uah";
+// b = "uah-usd";
 
-// let howMany = prompt(`Сколько денег хотите перевести?`);
+// let howMany = prompt("Сколько денег хотите перевести?");
 
 // let USD = 25;
 // let UAH = 25;
@@ -87,142 +134,169 @@
 
 // switch (typeOperation) {
 //   case a:
-//     alert(first + `гривень`);
+//     alert(first + "гривень");
 //     break;
 //   case b:
-//     alert(second + `долларов`);
+//     alert(second + "долларов");
 //     break;
 //   default:
-//     alert(`вы не ввели во что куда!`);
+//     alert("вы не ввели во что куда!");
 // }
 
 // task 9
 
-// let greeting = "да прибудет с тобой сила!";
-// let lang = prompt(`enter language please : en, ru, de`);
+// Создайте две переменные: greeting и lang.
+// Переменная greeting – пустая строка.Переменная lang может принимать три значения:
+// 'ru', 'en', 'de'(например, из prompt).Если она имеет значение 'ru',
+// то в переменную greeting запишите приветствие на русском языке,
+// если имеет значение 'en' – то на английском, если 'de' – на немецком.
+// Выведите на экран приветствие в зависимости от значения переменной lang.
+// Решите задачу через if-else и через switch-case.
 
-// let en = `en`,
-//   ru = `ru`,
-//   de = `de`;
+// let greeting = "да прибудет с тобой сила! ";
+// let lang = prompt(
+//   greeting + " for translate enter language please : en, ru, de"
+// );
+
+// let en = "en",
+//   ru = "ru",
+//   de = "de";
 // if (lang == en) {
-//   alert(greeting + ` may the force come with you!`);
+//   alert(" may the force come with you!");
 // } else if (lang == ru) {
-//   alert(greeting + ` да прибудет с тобой сила!`);
+//   alert(greeting);
 // } else if (lang == de) {
-//   alert(greeting + ` Möge die Macht mit dir sein!`);
+//   alert(" Möge die Macht mit dir sein!");
 // } else {
-//   alert(`Эта задача оказалась легче чем с именем`);
+//   alert("Эта задача оказалась легче чем с именем");
 // }
 
 // switch (lang) {
 //   case ru:
-//     alert(`да прибудет с тобой сила!`);
+//     alert("да прибудет с тобой сила!");
 //     break;
 //   case en:
-//     alert(`may the force come with you!`);
+//     alert("may the force come with you!");
 //     break;
 //   case de:
-//     alert(`Möge die Macht mit dir sein!`);
+//     alert("Möge die Macht mit dir sein!");
 //     break;
 //   default:
-//     alert(`Эта задача оказалась легче чем с именем`);
+//     alert("Эта задача оказалась легче чем с именем");
 // }
 
 // task 10
-// var x = +prompt(`Введите месяц `, 1);
-// var month = new Date().getMonth(x);
-// month++;
+
+// В переменной month хранится текущий месяц(new Date()).
+// Определите в какую пору года попадает этот месяц
+// (зима, лето, весна, осень).
+
+// var x = new Date().getMonth();
+// x++;
 // if (x <= 3) {
-//   alert(`сейчас зима`);
+//   alert("сейчас зима");
 // } else if (x > 3 && x <= 6) {
-//   alert(`сейчас весна`);
+//   alert("сейчас весна");
 // } else if (x > 6 && x <= 9) {
-//   alert(`сейчас лето`);
+//   alert("сейчас лето");
 // } else {
-//   alert(`сейчас осень`);
+//   alert("сейчас осень");
 // }
 
 // task 11
+
+// Переменная lang может принимать два значения: 'ru' и 'en'.
+// Переменная day принимает значение от 0 до 6 - ти.
+// Если lang имеет значение 'ru',
+// то в переменную result запишите название дня недели на русском языке
+// в соответствии со значением переменной day(0 – воскресенье,
+// 1 – понедельник, 2 – вторник и т.д.).
+// Если же lang имеет значение 'en' – то аналогично,
+// но день недели будет на английском.
+
 // debugger;
-// let lang = prompt(`выберите язык : ру, en`);
-// let ru = `ру`;
-// let en = `en`;
+// let lang = prompt("выберите язык : ру, en");
+// let ru = "ру";
+// let en = "en";
 // let dayEn, day;
-// let def = `введите цифру 0-6! / choose number 0-6!`;
+// let def = "введите цифру 0-6! / choose number 0-6!";
 
 // if (lang == ru) {
-//   day = +prompt(`выберите день от 0 - 6. Если что воскресенье = 0 )`);
+//   day = +prompt("выберите день от 0 - 6. Если что воскресенье = 0 )");
 // } else if (lang == en) {
-//   dayEn = +prompt(`select a day 0 - 6.If that Sunday = 0)`);
+//   dayEn = +prompt("select a day 0 - 6.If that Sunday = 0)");
 // } else {
-//   alert(`вы не выбрали`);
+//   alert("вы не выбрали");
 // }
 
 // switch (day) {
 //   case 0:
-//     day = alert(`Сегодня воскресенье`);
+//     day = alert("Сегодня воскресенье");
 //     break;
 //   case 1:
-//     day = alert(`Сегодня понедельник`);
+//     day = alert("Сегодня понедельник");
 //     break;
 //   case 2:
-//     day = alert(`Сегодня вторник`);
+//     day = alert("Сегодня вторник");
 //     break;
 //   case 3:
-//     day = alert(`Сегодня среда`);
+//     day = alert("Сегодня среда");
 //     break;
 //   case 4:
-//     day = alert(`Сегодня четверг`);
+//     day = alert("Сегодня четверг");
 //     break;
 //   case 5:
-//     day = alert(`Сегодня пятница`);
+//     day = alert("Сегодня пятница");
 //     break;
 //   case 6:
-//     day = alert(`Сегодня суббота`);
+//     day = alert("Сегодня суббота");
 //     break;
 // }
 
 // switch (dayEn) {
 //   case 0:
-//     day = alert(`Today is sunday`);
+//     day = alert("Today is sunday");
 //     break;
 //   case 1:
-//     day = alert(`Today is monday`);
+//     day = alert("Today is monday");
 //     break;
 //   case 2:
-//     day = alert(`Today is tuesday`);
+//     day = alert("Today is tuesday");
 //     break;
 //   case 3:
-//     day = alert(`today is wednesday`);
+//     day = alert("today is wednesday");
 //     break;
 //   case 4:
-//     day = alert(`today is thursday`);
+//     day = alert("today is thursday");
 //     break;
 //   case 5:
-//     day = alert(`today is friday`);
+//     day = alert("today is friday");
 //     break;
 //   case 6:
-//     day = alert(`today is saturday`);
+//     day = alert("today is saturday");
 //     break;
 // }
 
 // if (day >= 7) {
 //   alert(def);
 // } else {
-//   alert(`Конец/the end`);
+//   alert("Конец/the end");
 // }
 
 //task 12
+
+// Исправить код
+
 // debugger;
-// var x = +prompt(`x`, 0);
+// var x = +prompt("x", 0);
 // var error = null;
 
 // switch (x) {
 //   case 0:
-//     error = `на ноль делить нельзя`;
+//     error = "на ноль делить нельзя";
 //     break;
 //   case 1:
-//     error = `На единицу делить бессмысленно`;
+//     error = "На единицу делить бессмысленно";
 //     break;
 // }
 
